da5a426d563bc9b8f8f77a1e6b522b66e76a060b
FrankysWeb <86435356+FrankysWeb@users.noreply.github.com>
2026-09-24 15:46:09 +0200

    Disable CredentialGuard
    Script restructured and option to disable CredentialGuard added

--- Apply-ExchangeHealthChecker-Recommendations.ps1
+++ Apply-ExchangeHealthChecker-Recommendations.ps1
@@ -2,77 +2,57 @@
 Param (
     # Configure static 32GB Pagefile
     # https://aka.ms/HC-PageFile
-    [Parameter(Mandatory = $true, Position = 0, HelpMessage = "Configure static 32GB Pagefile (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetStaticPagefile,
+    [switch]$SetStaticPagefile,
 
     # Diable NIC power saving
     # https://microsoft.github.io/CSS-Exchange/Diagnostics/HealthChecker/SleepyNICCheck/
-    [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Diable NIC power saving (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetDisableNicPowersaving,
+    [switch]$SetDisableNicPowersaving,
 
     # Set Power Plan to High Performance
-    [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Set Power Plan to High Performance (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetPowerPlanToHighPerformance,
+    [switch]$SetPowerPlanToHighPerformance,
 
     # Set TCP KeepAliveTime to 30min
     # https://aka.ms/HC-TcpIpSettingsCheck
-    [Parameter(Mandatory = $true, Position = 3, HelpMessage = "Set TCP KeepAliveTime to 30min (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetTCPKeepAliveTimeTo30Min,
+    [switch]$SetTCPKeepAliveTimeTo30Min,
 
     # Configure TLS settings
     # https://aka.ms/HC-TLSConfigDocs
-    [Parameter(Mandatory = $true, Position = 4, HelpMessage = "Configure TLS settings (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetTlsSettings,
+    [switch]$SetTlsSettings,
 
     # Configure Download Domains to Autodiscover Hostname
     # https://microsoft.github.io/CSS-Exchange/Diagnostics/HealthChecker/DownloadDomainCheck/
-    [Parameter(Mandatory = $true, Position = 5, HelpMessage = "Configure Download Domains to Autodiscover Hostname (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetDownloadDomains,
+    [switch]$SetDownloadDomains,
 
     # Disable OutlookAnywhere SSL Offloading
     # required for Windows Extended Protection
-    [Parameter(Mandatory = $true, Position = 6, HelpMessage = "Disable OutlookAnywhere SSL Offloading (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetOASslOffloadingToFalse,
+    [switch]$SetOASslOffloadingToFalse,
 	
 	# SMB1 uninstall
     # Error "SMB1 Installed = true" and "SMB1 Blocked = false"
-    [Parameter(Mandatory = $true, Position = 7, HelpMessage = "Uninstall and disable SMB1? (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetSMB1Uninstall,
+    [switch]$SetSMB1Uninstall,
 	
 	# MSMQuninstall
     # Warning "MSMQ Windows Feature Installed"
-    [Parameter(Mandatory = $true, Position = 8, HelpMessage = "Uninstall MSMQ Windows Feature? (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetMSMQ,
+    [switch]$SetMSMQ,
 
 	# IanaTimeZoneMappings.xml invalid
 	# Correct IanaTimeZoneMappings.xml with external MS Script
-    [Parameter(Mandatory = $true, Position = 9, HelpMessage = "Correct duplicate entries in IanaTimeZoneMappings.xml? (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetIanaTimeZoneMappings,
+    [switch]$SetIanaTimeZoneMappings,
 
     # Configure Windows Extended Protection
     # https://microsoft.github.io/CSS-Exchange/Security/Extended-Protection/
-    [Parameter(Mandatory = $true, Position = 10, HelpMessage = "Configure Windows Extended Protection (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetExchangeExtendedProtection,
+    [switch]$SetExchangeExtendedProtection,
 	
 	# Configure PowerShell serialization payload feature?
     # https://microsoft.github.io/CSS-Exchange/Diagnostics/HealthChecker/SerializedDataSigningCheck/
-    [Parameter(Mandatory = $true, Position = 11, HelpMessage = "Configure PowerShell serialization payload feature (y/n)?")]
-    [ValidateSet("y","n")]
-    [string]$SetPowerShellSerializationPayload
+    [switch]$SetPowerShellSerializationPayload,
+
+    # Disable Credential Guard (not supported by Exchange Server)
+    # https://learn.microsoft.com/de-de/windows/security/identity-protection/credential-guard/configure?tabs=reg
+    [switch]$SetDisableCredentialGuard
 )
 Process {
-  if ($SetStaticPagefile -eq "y") {
+  if ($SetStaticPagefile) {
 
     $PageFileSizeMB = (Get-WMIObject -class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | ForEach-Object {[Math]::Round(($_.sum / 1MB),2)*0.25})
     
@@ -86,20 +66,20 @@ Process {
     $pagefileset.Put() | Out-Null
   }
 
-  if ($SetDisableNicPowersaving -eq "y") {
+  if ($SetDisableNicPowersaving) {
     New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001" -Name "PnPCapabilities" -Value 280 -PropertyType "DWord" -Force
   }
 
-  if ($SetPowerPlanToHighPerformance -eq "y") {
+  if ($SetPowerPlanToHighPerformance) {
     $PowerPlan = Get-WmiObject -Namespace root\cimv2\power -Class Win32_PowerPlan -Filter "InstanceID = 'Microsoft:PowerPlan\\{8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c}'"
     $PowerPlan.Activate()
   }
   
-  if ($SetTCPKeepAliveTimeTo30Min -eq "y") {
+  if ($SetTCPKeepAliveTimeTo30Min) {
     New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\Tcpip\Parameters" -Name "KeepAliveTime" -Value 1800000 -PropertyType DWord -Force
   }
 
-  if ($SetTlsSettings -eq "y") {
+  if ($SetTlsSettings) {
     # Enable TLS 1.2
     If (!(Test-Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server")) {
       New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server" -Force
@@ -178,39 +158,51 @@ Process {
     New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Client" -Name "DisabledByDefault" -Value 1 -PropertyType "DWord" -Force
   }
   
-  if ($SetDownloadDomains -eq "y") {
+  if ($SetDownloadDomains) {
     $DownloadDomain = (Get-ClientAccessService).AutoDiscoverServiceInternalUri.Host
     Set-OwaVirtualDirectory -Identity "owa (default Web site)" -ExternalDownloadHostName $DownloadDomain -InternalDownloadHostName $DownloadDomain
     Set-OrganizationConfig -EnableDownloadDomains $true
   }
 
-  if ($SetOASslOffloadingToFalse -eq "y") {
+  if ($SetOASslOffloadingToFalse) {
     Get-OutlookAnywhere -Server $env:computername | Set-OutlookAnywhere -SSLOffloading $false -InternalClientsRequireSsl $true -ExternalClientsRequireSsl $true
   }
   
-  if ($SetSMBUninstall -eq "y") {
+  if ($SetSMB1Uninstall) {
     Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart
 	Set-SmbServerConfiguration -EnableSMB1Protocol $false -Confirm:$false
   }
 
-  if ($SetMSMQ -eq "y") {
+  if ($SetMSMQ) {
     Remove-WindowsFeature NET-WCF-MSMQ-Activation45,MSMQ -Confirm:$false
   }
 
-  if ($SetExchangeExtendedProtection -eq "y") {
+  if ($SetExchangeExtendedProtection) {
     $ScriptPath = "https://github.com/microsoft/CSS-Exchange/releases/latest/download/ExchangeExtendedProtectionManagement.ps1"
     Invoke-WebRequest -Uri $ScriptPath -outfile "ExchangeExtendedProtectionManagement.ps1"
     .\ExchangeExtendedProtectionManagement.ps1
   }
   
-  if ($SetPowerShellSerializationPayload -eq "y") {
+  if ($SetPowerShellSerializationPayload) {
 	New-SettingOverride -Name "EnableSigningVerification" -Component Data -Section EnableSerializationDataSigning -Parameters @("Enabled=true") -Reason "Enabling Signing Verification"
 	Get-ExchangeDiagnosticInfo -Process Microsoft.Exchange.Directory.TopologyService -Component VariantConfiguration -Argument Refresh
   }
 
-  if ($SetIanaTimeZoneMappings -eq "y") {  
+  if ($SetIanaTimeZoneMappings) {  
     $ScriptPath = "https://github.com/microsoft/CSS-Exchange/releases/latest/download/Remove-DuplicateEntriesFromIanaMappings.ps1"
     Invoke-WebRequest -Uri $ScriptPath -outfile "Remove-DuplicateEntriesFromIanaMappings.ps1"
     .\Remove-DuplicateEntriesFromIanaMappings.ps1
+  }
+
+  if ($SetDisableCredentialGuard) {
+    If (!(Test-Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa")) {
+      New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Force
+    }
+    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -PropertyType "DWord" -Force
+
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard")) {
+      New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" -Force
+    }
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" -Name "LsaCfgFlags" -Value 0 -PropertyType "DWord" -Force
   }
 }
